1ef5549e8ceb8507e1a8c7b0c1ce37f98c195607
Jazmin Ivy <princessivy07@yahoo.com>
2023-02-17 11:56:30 -0600

    Updated robot.py file

--- robot.py
+++ robot.py
@@ -4,6 +4,7 @@ class Robot:
     def __init__(self,name):
         self.name = name
         self.health = 100
+        self.active_weapon = Weapon
 
     def attack(self, dinosaur):
         print(f"{self.name} attacked {dinosaur}")
