a12d0f5c736aa118f09a9bc486516ae4d1eb75f2
Jazmin Ivy <princessivy07@yahoo.com>
2023-02-17 11:10:43 -0600

    Updated battlefield.py file

---
+++ battlefield.py
@@ -0,0 +1,18 @@
+from dinosaur import Dinosaur
+from robot import Robot
+
+class Battlefield:
+    def __init__(self):
+        pass
+
+    def run_game(self):
+        pass
+
+    def display_welcome(self):
+        print("Welcome to Robots and Dinosaurs!\nOnly one side can win!\n")
+
+    def battle_phase(self):
+        pass
+
+    def display_winner(self):
+        pass
