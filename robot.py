76022c367f8b6a5c18beef10c81fd4cfafa11ee1
Jazmin Ivy <princessivy07@yahoo.com>
2023-02-20 13:52:45 -0600

    Updated robot.py file

--- robot.py
+++ robot.py
@@ -1,10 +1,10 @@
-from weapon import Weapon
+import weapon
 
 class Robot:
     def __init__(self,name):
         self.name = name
         self.health = 100
-        self.active_weapon = Weapon
+        self.weapon = weapon.Weapon("Pistol", )
 
     def attack(self, dinosaur):
         print(f"{self.name} attacked {dinosaur}")
