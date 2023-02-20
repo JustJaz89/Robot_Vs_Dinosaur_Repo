9fd3b9c9cffb5876d1d564a0c91a6bc23c1cf488
Jazmin Ivy <princessivy07@yahoo.com>
2023-02-20 13:45:17 -0600

    Updated dinosaur.py file

--- dinosaur.py
+++ dinosaur.py
@@ -4,6 +4,16 @@ class Dinosaur:
         self.health = 100
         self.attack_power = attack_power
         self.attack_type = ("Claw", "Bite", "Stomp")
+        self.attack = ""
 
     def attack(self, robot):
-        pass
+        self.select_attack()
+        robot.health = robot.health - int(self.attack_power)
+        print(f"\n{self.name} attacked {robot.name} with {self.attack} for {self.attack_power} damage!\n{robot.name}'s health is now {robot.health}")
+        if (robot.health(self)):
+            print("\nSelect an attack: ")
+            i = 1
+            for attack in self.attacks:
+                print(f"{i}: {attack}")
+                i += 1
+            self.attack = self.attacks[int(input("\nEnter number: ")) - 1]
