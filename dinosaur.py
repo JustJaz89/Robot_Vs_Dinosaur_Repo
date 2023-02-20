6f558eb45c9794fa36dd40cfbbcc5bf79a331b13
Jazmin Ivy <princessivy07@yahoo.com>
2023-02-20 13:59:50 -0600

    Updated dinosaur.py file

--- dinosaur.py
+++ dinosaur.py
@@ -1,19 +1,22 @@
 class Dinosaur:
-    def __init__(self, name, attack_power):
+    def __init__(self, name, attack_power: int):
         self.name = name
         self.health = 100
         self.attack_power = attack_power
-        self.attack_type = ("Claw", "Bite", "Stomp")
+        self.attacks = ("Claw", "Bite", "Stomp")
         self.attack = ""
 
-    def attack(self, robot):
+    def attack_robot(self, robot):
         self.select_attack()
         robot.health = robot.health - int(self.attack_power)
-        print(f"\n{self.name} attacked {robot.name} with {self.attack} for {self.attack_power} damage!\n{robot.name}'s health is now {robot.health}")
-        if (robot.health(self)):
-            print("\nSelect an attack: ")
-            i = 1
-            for attack in self.attacks:
-                print(f"{i}: {attack}")
-                i += 1
-            self.attack = self.attacks[int(input("\nEnter number: ")) - 1]
+        print(f"{self.name} attacked {robot.name} with {self.attack} for {self.attack_power} damage!\n{robot.name}'s health is now {robot.health}")
+        if (robot.health) <= 0:
+            print(f"\n{robot.name} has been destroyed!")
+
+    def select_attack(self):
+        print("\nSelect an attack: ")
+        i = 1
+        for attack in self.attacks:
+            print(f"{i}: {attack}")
+            i += 1
+        self.attack = self.attacks(int(input("\nEnter number: ")))
