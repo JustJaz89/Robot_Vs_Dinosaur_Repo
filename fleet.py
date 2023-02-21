45f8e4e57cbf2b521a2c4f99a1a7838e1c5cbedc
Jazmin Ivy <princessivy07@yahoo.com>
2023-02-21 09:05:08 -0600

    Updated fleet.py file

--- fleet.py
+++ fleet.py
@@ -1,6 +1,6 @@
 class Fleet:
     def __init__(self):
-        self.robots = ["megatron", "r2d2", "t_800"]
+        self.robots = ["Megatron", "R2D2", "T_800"]
 
     def create_fleet(self, robots):
         for robot in robots:
