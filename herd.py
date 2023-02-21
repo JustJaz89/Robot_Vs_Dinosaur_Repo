96124fb1a4a7b63e3ea493fbd77b5b70c49e1be4
Jazmin Ivy <princessivy07@yahoo.com>
2023-02-21 08:47:53 -0600

    Updated herd.py file

--- herd.py
+++ herd.py
@@ -1,6 +1,6 @@
 class Herd:
     def __init__(self):
-        self.dinosaurs = []
+        self.dinosaurs = ["t_rex", "raptor", "spino"]
 
     def create_herd(self, dinosaurs):
         for dinosaur in dinosaurs:
